5b1753342e07ae1094253d72ef940e419081295e
TMTron <martin.trummer@tmtron.com>
2018-02-02 14:12:26 +0100

    added a check method and a lazy attribute to DsInstanceBase

--- src/main/java/com/tmtron/ex/immutables/DsInstanceBase.java
+++ src/main/java/com/tmtron/ex/immutables/DsInstanceBase.java
@@ -1,9 +1,23 @@
 package com.tmtron.ex.immutables;
 
+import org.immutables.value.Value;
+
 public abstract class DsInstanceBase {
 
     public abstract String computerName();
 
     public abstract String macAddress();
+
+    @Value.Check
+    protected void check() {
+        if (computerName().isEmpty()) {
+            throw new RuntimeException("ComputerName must not be empty");
+        }
+    }
+
+    @Value.Lazy
+    String computerNameUpper() {
+        return computerName().toUpperCase();
+    }
 
 }
